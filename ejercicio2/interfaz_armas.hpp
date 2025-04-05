1a5deff300636272c8944e17e7aa1d4522bb4845
agustinpatruno <agustinpatruno@gmail.com>
2025-04-05 17:06:48 -0300

    avance en el ejercicio 2

--- ejercicio2/interfaz_armas.hpp
+++ ejercicio2/interfaz_armas.hpp
@@ -6,7 +6,39 @@ using namespace std;
 class arma
 {
     public:
-        virtual ~arma(/* args */) = default;
+
+        virtual void mostrar_tipo_arma() = 0;
+
+        virtual void mostrar_daño() = 0;
+
+        virtual int mostrar_cantidad_de_usos() = 0;
+
+        virtual void restar_usos() = 0;
+
+        virtual void mostrar_golpes_disponibles() = 0;
+
+        virtual void mostrar_rareza() = 0;
+
+        virtual void mostrar_golpes_extra() = 0;
+};
+
+class items_magicos: public arma
+{
+    private:
+        
+        string tipo_arma;
+
+        string nivel_rareza;
+
+        double daño;
+
+        int cantidad_golpes_disponibles;
+
+        int golpes_extra;
+
+    public:
+
+        items_magicos(string tip_arma, string rareza, double daño_arma, int cant_golpes, int cant_golpes_extra);
 
         virtual void mostrar_tipo_arma();
 
@@ -16,9 +48,42 @@ class arma
 
         virtual void restar_usos();
 
-        virtual void restar_daño();
+        void mostrar_golpes_disponibles();
 
+        void mostrar_rareza();
+
+        void mostrar_golpes_extra();
 };
-arma::arma(/* args */)
+
+class armas_combate
 {
-}
+    private:
+
+        string tipo_arma;
+
+        string nivel_rareza;
+
+        double daño;
+
+        int cantidad_golpes_disponibles;
+
+        int golpes_extra;
+
+    public:
+
+        armas_combate(string tip_arma, string rareza, double daño_arma, int cant_golpes, int cant_golpes_extra);
+
+        virtual void mostrar_tipo_arma();
+
+        virtual void mostrar_daño();
+
+        virtual int mostrar_cantidad_de_usos();
+
+        virtual void restar_usos();
+
+        void mostrar_golpes_disponibles();
+
+        void mostrar_rareza();
+
+        void mostrar_golpes_extra();
+};
---
+++ ejercicio2/interfaz_personajes.hpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <string>
+#include <memory>
+#include "interfaz_armas.hpp"
+using namespace std;
+
+class personaje
+{
+    public:
+
+        virtual void atacar(personaje otro) = 0;
+
+        virtual void mostrar_tipo_personaje() = 0;
+        
+        virtual void usar_habilidad_especial() = 0;
+
+        virtual void mostrar_habilidad_especial() = 0;
+
+        virtual void mostrar_hp() = 0;
+
+};
+
+class mago : public personaje
+{
+    private:
+
+        string tipo_mago;
+
+        string hechizo_especial;
+
+        double daño_hechizo_especial;
+
+        int cant_usos_hechizo_especial;
+
+        double hp;
+
+        shared_ptr<arma> tipo_arma;
+
+    public:
+
+        mago(string mago, string hechizo_especial, int daño_hechizo_especial, int cant_usos_hechizo_especial, double hp, double daño_hechizo);
+
+        virtual void atacar(personaje otro);
+
+        virtual void mostrar_tipo_personaje();
+        
+        virtual void usar_habilidad_especial();
+
+        virtual void mostrar_habilidad_especial();
+
+        virtual void mostrar_hp();
+
+        void mostrar_daño_hechizo_especial();
+
+        void mostrar_cant_golpes_especial();
+};
+
+class guerrero
+{
+    private:
+
+        string tipo_guerrero;
+
+        string golpe_especial;
+
+        double daño_golpe_especial;
+
+        int cant_usos_golpe_especial;
+
+        double hp;
+
+        shared_ptr<arma> tipo_arma;
+
+    public:
+
+        guerrero(string guerrero, string golpe_especial, double daño_golpe_especial, int cant_golpes_especial, double hp, double daño_golpe);
+
+        virtual void atacar(personaje otro) = 0;
+
+        virtual void mostrar_tipo_personaje() = 0;
+        
+        virtual void usar_habilidad_especial() = 0;
+
+        virtual void mostrar_habilidad_especial() = 0;
+
+        virtual void mostrar_hp();
+
+        void mostrar_daño_golpe_especial();
+
+        void mostrar_cant_golpes_especial();
+};
+
+
