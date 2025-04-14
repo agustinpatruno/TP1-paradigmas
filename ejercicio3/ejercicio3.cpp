690c57f773b7626670141cd99d9e85f2987edc80
agustinpatruno <agustinpatruno@gmail.com>
2025-04-13 21:32:47 -0300

    casi finalizacion del ejercicio 3

--- ejercicio3/ejercicio3.cpp
+++ ejercicio3/ejercicio3.cpp
@@ -12,6 +12,11 @@ shared_ptr<arma> PersonajeFactory::crear_arma_factory(armas_totales tipo_arma, f
     double daño = daño_magicos_combate[static_cast<size_t>(tipo_arma)];
     
    nueva_arma = crear_arma(tipo_arma, dato1, dato2, dato3, daño);
+
+   if (!nueva_arma)
+    {
+        return nullptr;
+    }
     
    shared_ptr<arma> arma_shared = move(nueva_arma);
 
@@ -24,7 +29,7 @@ shared_ptr<personaje> PersonajeFactory::crear_personaje_factory(personajes_total
     {
         shared_ptr<personaje> nuevo_personaje;
 
-        if (hechicero3 <= perso <= nigromante3)
+        if (hechicero3 <= perso && perso <= nigromante3)
         {
             int numero_mago = static_cast<int>(perso);
      
@@ -46,7 +51,7 @@ shared_ptr<personaje> PersonajeFactory::crear_personaje_factory(personajes_total
                     break;
             }
         }
-        else if (barbaro3 <= perso <= gladiador3)
+        else if (barbaro3 <= perso && perso <= gladiador3)
         {
             int numero_guerrero = static_cast<int>(perso);
      
@@ -55,25 +60,26 @@ shared_ptr<personaje> PersonajeFactory::crear_personaje_factory(personajes_total
             switch (tipo_guerrero)
             {
                 case a_barbaro:
-                    nuevo_personaje = make_shared<barbaro>(hab_especial, dato1, 0, dato_personal);
+                    nuevo_personaje = make_shared<barbaro>(hab_especial, dato1, 2, dato_personal);
                     break;
                 case a_paladin:
-                    nuevo_personaje = make_shared<paladin>(hab_especial, dato1, 0, dato_personal);
+                    nuevo_personaje = make_shared<paladin>(hab_especial, dato1, 2, dato_personal);
                     break;
                 case a_caballero:
-                    nuevo_personaje = make_shared<caballero>(hab_especial, dato1, 0, dato_personal);
+                    nuevo_personaje = make_shared<caballero>(hab_especial, dato1, 2, dato_personal);
                     break;
                 case a_mercenario:
-                    nuevo_personaje = make_shared<mercenario>(hab_especial, dato1, 0, dato_personal);
+                    nuevo_personaje = make_shared<mercenario>(hab_especial, dato1, 2, dato_personal);
                     break;
                 case a_gladiador:
-                    nuevo_personaje = make_shared<gladiador>(hab_especial, dato1, 0, dato_personal);
+                    nuevo_personaje = make_shared<gladiador>(hab_especial, dato1, 2, dato_personal);
                     break;
             }
         }
         else
         {
             throw logic_error("error, ese tipo de personaje no existe");
+            return nullptr;
         }
         return nuevo_personaje;
     }
@@ -81,6 +87,7 @@ shared_ptr<personaje> PersonajeFactory::crear_personaje_factory(personajes_total
     {
         std::cerr << e.what() << '\n';
     }
+    return nullptr;
 }
 
 shared_ptr<personaje> PersonajeFactory::crear_personaje_armado_factory(personajes_totales perso,hab_totales hab_especial, int dato1, float dato_personal, armas_totales tip_arma, float dato2, float dato3, float dato4)
@@ -90,15 +97,28 @@ shared_ptr<personaje> PersonajeFactory::crear_personaje_armado_factory(personaje
     shared_ptr<personaje> nuevo_personaje = perso_factory -> crear_personaje_factory(perso, hab_especial, dato1, dato_personal);
 
     nuevo_personaje -> agregar_arma(tip_arma, dato2, dato3, dato4);
+
+    return nuevo_personaje;
 }
 
 void crear_elementos()
 {
+    shared_ptr<PersonajeFactory> perso_factory = make_shared<PersonajeFactory>();
 
+    shared_ptr<arma> arma = perso_factory ->crear_arma_factory(arma_baston, 5, 4, 3);
+
+    arma ->Get_infoarma_general();
+
+    shared_ptr<personaje> crear_personaje = perso_factory ->crear_personaje_factory(hechicero3, Explosion_arcana,7, 8);
     
+    crear_personaje ->mostrar_info_personaje();
+    crear_personaje ->mostrar_hp();
+    crear_personaje ->modificar_hp(15);
+    cout << "hp: " << crear_personaje ->retornar_hp() << endl;
 }
 
 int main()
 {
+    crear_elementos();
     return 0;
 }
